2b5d8044342fb28f8e537105ff8da017a7b41f55
Salamanca <hernan.s.salamanca@accenture.com>
2023-04-24 11:21:54 -0500

    Proyecto citas veterinaria, MVP. Curso React la guia completa.

--- README.md
+++ README.md
@@ -2,3 +2,48 @@
 Repositorio del curso React - la guia completa, del instructor Juan de la Torre
 
 Cambios en el repositorio santiagoPeer
+
+## Recuento de lo que voy haciendo
+
+
+## Comandos
+
+Crear proyecto
+```
+    npm init vite@latest // Para crear con vite
+
+    cd projectFolder
+
+    npm install
+
+    npm run dev
+
+```
+
+
+
+## Dependencias
+
+
+## Estilos con Tailwind
+
+Instalando Tailwind CSS:
+```
+    npm i -D tailwindcss postcss autoprefixer
+
+    npx tailwindcss init -p
+
+```
+
+En el archivo global de estilos "index.css":
+```
+    @tailwind base;
+    @tailwind components;
+    @tailwind utilities;
+```
+
+En el archivo tailwind.config.js:
+```
+    content: ["./index.html", "./src/**/*.jsx"]
+```
+
---
+++ citas-react/src/App.jsx
@@ -0,0 +1,58 @@
+import {useEffect, useState} from 'react';
+import Formulario from "./components/Formulario";
+import Header from "./components/Header";
+import ListadoPacientes from "./components/ListadoPacientes";
+
+
+function App() {
+
+  const [pacientes, setPacientes] = useState([]); // Guarda la info global
+  const [paciente, setPaciente] = useState({}); // Guarda la info para editar
+
+  // Manejo del local storage
+  useEffect( () => {
+    const obtenerLocalStorage = () => {
+      const pacientesLocalStorage = JSON.parse(localStorage.getItem('pacientes')) ?? [];
+      setPacientes(pacientesLocalStorage);
+    }
+  }, []);
+
+  useEffect( () => {
+    localStorage.setItem('pacientes', JSON.stringify(pacientes));
+  }, [pacientes])
+
+
+  const eliminarPaciente = (id) => {
+    const pacientesActualizados = pacientes.filter( (paciente) => {
+      return paciente.id !== id;
+    })
+
+    console.log(pacientesActualizados);
+  };
+
+  return (
+    <div className='container mx-auto mt-20'>
+      
+      <Header />
+      <div className="mt-12 md:flex">
+
+        <Formulario 
+          pacientes={pacientes}
+          setPacientes={setPacientes}
+          paciente={paciente}
+          setPaciente={setPaciente}
+        />
+
+        <ListadoPacientes 
+          pacientes={pacientes}       
+          setPaciente={setPaciente} 
+          eliminarPaciente={eliminarPaciente}  
+        />
+
+      </div>
+
+    </div>
+  )
+}
+ 
+export default App
